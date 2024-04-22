576f862f7b6ce91409b39ded32251eeb325470a1
Paulo Aguilar <desarrollo1@ideasgroup.com.ec>
2024-04-21 19:31:57 -0500

    version beta-01

---
+++ legalbot/chat_agent/document_retriever.py
@@ -0,0 +1,92 @@
+from langchain.vectorstores.faiss import FAISS
+from langchain_community.embeddings.openai import OpenAIEmbeddings
+from processing.document import Document, DocumentChunk
+from langchain_core.documents.base import Document as LangchainDocument
+
+import os
+
+EMBEDING_STORAGE = os.environ.get("EMBEDING_STORAGE")
+
+
+class DocumentRetriever:
+
+    DEFAULR_RETRIEVER_PROMPT = """Para tu respuesta, considera SOLAMENTE los siguientes extractos de normativa interna
+    y responde con el formato: 'De acuerdo al Art. (numero) del (nombre del documento) "(cita exacta)".
+    Documentos Relacionados: 
+    {input_docs}
+    """
+
+    def __init__(
+        self,
+        retriever_prompt_template=DEFAULR_RETRIEVER_PROMPT,
+        score_threshold=0.5,
+        k=3,
+    ):
+        self.template = retriever_prompt_template
+        self.score_threshold = score_threshold
+        self.embeddings = OpenAIEmbeddings()
+        self.__k = k
+        self.__indexes = self.__load_indexes()
+
+    def __load_indexes(self):
+        indexes = []
+        # iterate over the embeding storage directory
+        # and load the indexes
+        if not os.path.exists(EMBEDING_STORAGE):
+            raise Exception("No embeding storage found")
+        for filename in os.listdir(EMBEDING_STORAGE):
+            if filename.endswith(".faiss"):
+                indexes.append(
+                    FAISS.load_local(
+                        EMBEDING_STORAGE,
+                        self.embeddings,
+                        filename.replace(".faiss", ""),
+                    )
+                )
+        faiss_index = indexes[0]
+        for index in indexes[1:]:
+            faiss_index.merge_from(index)
+        return faiss_index
+
+    def search_documents(self, query):
+        return self.__indexes.similarity_search_with_score(
+            query,
+            k=self.__k,
+        )
+
+    def __parse_chunk(self, faiss_docs: LangchainDocument) -> list[DocumentChunk]:
+        chunks = []
+
+        for faiss_doc in faiss_docs:
+            metadata = faiss_doc.metadata
+            content = faiss_doc.page_content
+            chunks.append(DocumentChunk(content, metadata))
+
+        return chunks
+
+    def __filter_documents_by_score(
+        self, documents_with_scores: list[tuple[Document, float]]
+    ) -> list[LangchainDocument]:
+        documents = []
+        for document, score in documents_with_scores:
+            if score <= self.score_threshold:
+                documents.append(document)
+        return documents
+
+    def get_document_query(self, query: str):
+        documents_with_scores = self.search_documents(query)
+        documents = self.__filter_documents_by_score(documents_with_scores)
+        chunk = self.__parse_chunk(documents)
+        return chunk
+
+    def get_document_query_prompt(self, query: str):
+        chunks = self.get_document_query(query)
+        input_docs = "Documentos relevantes para la consulta: \n"
+
+        if not chunks:
+            input_docs += "No se encontraron resultados"
+
+        for chunk in chunks:
+            input_docs += chunk.content + "\n"
+
+        return self.template.format(input_docs=input_docs)
