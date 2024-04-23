89e8731a14f471298527db6aa696c216ea3029dc
Paulo Aguilar <pauloaguilar2001@gmail.com>
2024-04-23 06:29:06 +0000

    Revert "try logging"
    This reverts commit 76ac4e532c6d8dd7c1dfb9e22625cfd7f81120c4.

--- legalbot/app/config/settings.py
+++ legalbot/app/config/settings.py
@@ -157,28 +157,4 @@ if not DEBUG:
     INSTALLED_APPS.append('corsheaders')
     MIDDLEWARE.append('corsheaders.middleware.CorsMiddleware')
     CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(' ')
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': True,
-        'formatters': {
-            'verbose': {
-                'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
-            }
-        },
-        'handlers': {
-            'gunicorn': {
-                'level': 'DEBUG',
-                'class': 'logging.handlers.RotatingFileHandler',
-                'formatter': 'verbose',
-                'filename': '/opt/djangoprojects/reports/bin/gunicorn.errors',
-                'maxBytes': 1024 * 1024 * 100,  # 100 mb
-            }
-        },
-        'loggers': {
-            'gunicorn.errors': {
-                'level': 'DEBUG',
-                'handlers': ['gunicorn'],
-                'propagate': True,
-            },
-        }
-    }
+
