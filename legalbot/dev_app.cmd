6f5d1e78d8a061c04b6ca2e8b8aab227bf75516d
Paulo Aguilar <pauloaguilar2001@gmail.com>
2024-05-08 11:15:27 -0500

    added limit queries

--- legalbot/dev_app.cmd
+++ legalbot/dev_app.cmd
@@ -1,5 +1,5 @@
 
 cd app
-start cmd TITLE Worker /c "..\env\Scripts\celery.exe -A config.celery worker -l info --pool=solo & pause"
+start cmd TITLE Worker /c "..\..\env\Scripts\celery.exe -A config.celery worker -l info --pool=solo & pause"
 TIMEOUT 2
-start cmd TITLE app /c "..\env\Scripts\python.exe manage.py runserver & pause"
+start cmd TITLE app /c "..\..\env\Scripts\python.exe manage.py runserver & pause"
