45135b935359651a8a2139eec061d59a90906f2b
Paulo Aguilar <pauloaguilar2001@gmail.com>
2024-08-13 08:31:04 -0500

    fixed windows dev error

--- legalbot/dev_app.cmd
+++ legalbot/dev_app.cmd
@@ -1,5 +1,5 @@
 
 cd app
-start cmd TITLE Worker /c "..\..\env\Scripts\python.exe ..\..\env\Scripts\celery.exe -A config.celery worker -l debug & pause"
+start cmd TITLE Worker /c "..\..\env\Scripts\python.exe ..\..\env\Scripts\celery.exe -A config.celery worker -l info  -P gevent & pause"
 TIMEOUT 2
 start cmd TITLE app /c "..\..\env\Scripts\python.exe manage.py runserver & pause"
